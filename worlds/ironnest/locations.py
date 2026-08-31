8aa54bcb45c8e689e307fc1e65e5bde7f31699dd
Jao247 <edu+github@celtickuma.com>
2026-09-01 00:14:29 +0100

    Successfully creating a room with this.

--- worlds/ironnest/locations.py
+++ worlds/ironnest/locations.py
@@ -23,6 +23,19 @@ LOCATION_NAME_TO_ID = {
     "Mission 1 - Medal 2 - Gold": 11,
     "Mission 1 - Medal 3 - Gold": 12,
     "Mission 1 - Medal 4 - Gold": 13,
+    "Mission 2": 21,
+    "Mission 2 - Medal 1 - Bronze": 22,
+    "Mission 2 - Medal 2 - Bronze": 23,
+    "Mission 2 - Medal 3 - Bronze": 24,
+    "Mission 2 - Medal 4 - Bronze": 25,
+    "Mission 2 - Medal 1 - Silver": 26,
+    "Mission 2 - Medal 2 - Silver": 27,
+    "Mission 2 - Medal 3 - Silver": 28,
+    "Mission 2 - Medal 4 - Silver": 29,
+    "Mission 2 - Medal 1 - Gold": 30,
+    "Mission 2 - Medal 2 - Gold": 31,
+    "Mission 2 - Medal 3 - Gold": 32,
+    "Mission 2 - Medal 4 - Gold": 33,
 }
 
 class IronNestLocation(Location):
@@ -38,7 +51,46 @@ def create_all_locations(world: IronNestWorld) -> None:
 
 
 def create_regular_locations(world: IronNestWorld) -> None:
+    iron_nest_map = world.get_region("IronNest Map")
     mission_1 = world.get_region("Mission 1")
+    mission_2 = world.get_region("Mission 2")
+
+    iron_nest_map_locations = get_location_names_with_ids(
+        ["Mission 1", "Mission 2"]
+    )
+    iron_nest_map.add_locations(iron_nest_map_locations, IronNestLocation)
+
+    mission_1_locations = get_location_names_with_ids([
+        "Mission 1 - Medal 1 - Bronze",
+        "Mission 1 - Medal 2 - Bronze",
+        "Mission 1 - Medal 3 - Bronze",
+        "Mission 1 - Medal 4 - Bronze",
+        "Mission 1 - Medal 1 - Silver",
+        "Mission 1 - Medal 2 - Silver",
+        "Mission 1 - Medal 3 - Silver",
+        "Mission 1 - Medal 4 - Silver",
+        "Mission 1 - Medal 1 - Gold",
+        "Mission 1 - Medal 2 - Gold",
+        "Mission 1 - Medal 3 - Gold",
+        "Mission 1 - Medal 4 - Gold",
+    ])
+    mission_1.add_locations(mission_1_locations, IronNestLocation)
+
+    mission_2_locations = get_location_names_with_ids([
+        "Mission 2 - Medal 1 - Bronze",
+        "Mission 2 - Medal 2 - Bronze",
+        "Mission 2 - Medal 3 - Bronze",
+        "Mission 2 - Medal 4 - Bronze",
+        "Mission 2 - Medal 1 - Silver",
+        "Mission 2 - Medal 2 - Silver",
+        "Mission 2 - Medal 3 - Silver",
+        "Mission 2 - Medal 4 - Silver",
+        "Mission 2 - Medal 1 - Gold",
+        "Mission 2 - Medal 2 - Gold",
+        "Mission 2 - Medal 3 - Gold",
+        "Mission 2 - Medal 4 - Gold",
+    ])
+    mission_2.add_locations(mission_2_locations, IronNestLocation)
 
 
 #def create_events(world: IronNestWorld) -> None:
--- worlds/ironnest/regions.py
+++ worlds/ironnest/regions.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from BaseClasses import Entrance, Region
+from rule_builder.rules import HasAll
 
 if TYPE_CHECKING:
     from .world import IronNestWorld
@@ -15,13 +16,20 @@ def create_and_connect_regions(world: IronNestWorld) -> None:
 
 
 def create_all_regions(world: IronNestWorld) -> None:
+    iron_nest_map = Region("IronNest Map", world.player, world.multiworld)
     mission_1 = Region("Mission 1", world.player, world.multiworld)
+    mission_2 = Region("Mission 2", world.player, world.multiworld)
 
-    regions = [mission_1]
+    regions = [iron_nest_map, mission_1, mission_2]
 
     world.multiworld.regions += regions
 
 
 def connect_regions(world: IronNestWorld) -> None:
+    iron_nest_map = world.get_region("IronNest Map")
     mission_1 = world.get_region("Mission 1")
+    mission_2 = world.get_region("Mission 2")
+
+    iron_nest_map.connect(mission_1, "Mission 1", HasAll("Mission 1 Briefing"))
+    iron_nest_map.connect(mission_2, "Mission 2", HasAll("Mission 2 Briefing"))
 
